878096ad50ddac2b379c14be11445dccadb19472
davidalencar <davidalencarignacio@gmail.com>
2020-08-05 22:14:10 -0300

    refactoring

--- src/startup/routers.js
+++ src/startup/routers.js
@@ -1,9 +1,7 @@
-const express = require('express');
 
 const store = require('../app/store/store.routers')
 
 module.exports = (app) => {
-    app.use(express.json())
 
     app.use('/api/stores', store)
 }
