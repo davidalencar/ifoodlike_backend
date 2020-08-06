7cf8107c62527a972bef65b52446f6848a8ce153
davidalencar <davidalencarignacio@gmail.com>
2020-08-05 21:04:48 -0300

    unit store name

--- src/app/store/store.model.js
+++ src/app/store/store.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const storeSchema = new mongoose.Schema({
     name: {
         type: String,
+        unique: true,
         trim: true,
         required: true,
         maxlength: 60       
