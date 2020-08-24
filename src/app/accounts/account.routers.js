00e741b8c74185aea85dce737bdfe7e2b9ecef00
davidalencar <davidalencarignacio@gmail.com>
2020-08-24 14:59:29 -0300

    user sec

--- src/app/accounts/account.routers.js
+++ src/app/accounts/account.routers.js
@@ -13,10 +13,10 @@ router.post('/', async (req, res) => {
 	if (error) return res.status(400).send(error.details[0].message)
 
 	let user = await userService.get(req.body.email)
-	if (!user) return res.status(400).send('Invalid email or password.')
+	if (!user) return res.status(401).send('Invalid email or password.')
 
 	const validPassword = await bcrypt.compare(req.body.password, user.password)
-	if (!validPassword) return res.status(400).send('Invalid email or password.')
+	if (!validPassword) return res.status(401).send('Invalid email or password.')
 
 	const access_token = await userService.generateAuthToken(user)
 	const stores = await storeService.getByUser(user._id.toString())
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,19 +1,25 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const storeService = require('../app/store/store.service')
 
 module.exports = function (req, res, next) {
 	const token = req.header("Authorization");
 	if (!token) return res.status(401).send("Access deined. No token provided.");
 
 	try {
-		
-		const user = jwt.verify(token, config.get("jwtPrivateKey"));	
-		
-		if (!user.stores.includes(req.params.id)) res.status(401).send("Access deined.");
 
-		req.user = user;
+		const user = jwt.verify(token, config.get("jwtPrivateKey"));
 
-		next();
+		storeService.getByUser(user.id)
+			.then(stores => {
+				if (!stores.includes(req.params.id)){
+					res.status(401).send("Access deined.")	
+				} else {
+					req.user = user;
+				} 
+				
+				next();
+			})
 	} catch (ex) {
 		res.status(400).send("Invalid token.");
 	}
