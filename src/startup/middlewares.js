ef21241dc5569a03edb709cb883cb6a083ae4891
davidalencar <davidalencarignacio@gmail.com>
2020-09-08 10:22:40 -0300

    point bslista.com

--- src/startup/middlewares.js
+++ src/startup/middlewares.js
@@ -4,7 +4,7 @@ const compression = require("compression")
 const cors = require('cors')
 
 const corsOptions = {
-   origin: [/\.bslista\.com$/]
+    origin: [/bslista\.com$/]
 }
 
 module.exports = function (app) {
